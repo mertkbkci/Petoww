3d824062b7be1e8f3e0912371d1a1fcae1d852fa
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-07 16:21:26 +0300

    initial commit

--- app/src/main/java/com/camerba/petoww/RegisterActivity.java
+++ app/src/main/java/com/camerba/petoww/RegisterActivity.java
@@ -3,12 +3,29 @@ package com.camerba.petoww;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText name;
+    private EditText email;
+    private EditText password;
+
+    private DatabaseReference mRootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        mRootRef = FirebaseDatabase.getInstance().getReference();
     }
 }
