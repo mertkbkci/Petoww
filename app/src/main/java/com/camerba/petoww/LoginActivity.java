3341e38725eb1fcfb4b76d260012ba0f24638fe9
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-07 19:38:44 +0300

    initial commit

--- app/src/main/java/com/camerba/petoww/LoginActivity.java
+++ app/src/main/java/com/camerba/petoww/LoginActivity.java
@@ -3,12 +3,31 @@ package com.camerba.petoww;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private TextView registerUser;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        registerUser = findViewById(R.id.register_user);
+
+
     }
 }
