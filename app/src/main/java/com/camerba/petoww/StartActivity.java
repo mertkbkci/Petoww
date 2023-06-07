06bbfa7368dc7050420cab4f41a7fbe9638bbf22
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-07 14:01:44 +0300

    initial commit

--- app/src/main/java/com/camerba/petoww/StartActivity.java
+++ app/src/main/java/com/camerba/petoww/StartActivity.java
@@ -3,12 +3,61 @@ package com.camerba.petoww;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class StartActivity extends AppCompatActivity {
+
+    private ImageView logoImage;
+    private LinearLayout linearLayout;
+    private Button register;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        logoImage = findViewById(R.id.logo_image);
+        linearLayout = findViewById(R.id.lineer_layout);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+
+
+        linearLayout.animate().alpha(0f).setDuration(1);
+
+            TranslateAnimation animation = new TranslateAnimation(0,0,0,-1000);
+                animation.setDuration(1000);
+                animation.setFillAfter(false);
+                animation.setAnimationListener(new MyAnimationListener());
+
+                logoImage.setAnimation(animation);
+    }
+
+    private class MyAnimationListener implements Animation.AnimationListener{
+
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+            logoImage.clearAnimation();
+            logoImage.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(1000);
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
