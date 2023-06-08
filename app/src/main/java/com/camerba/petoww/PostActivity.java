122bd1fb10193c1560f12db1b419b335e0682307
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-08 12:05:00 +0300

    initial commit

--- app/src/main/java/com/camerba/petoww/PostActivity.java
+++ app/src/main/java/com/camerba/petoww/PostActivity.java
@@ -1,14 +1,39 @@
 package com.camerba.petoww;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PostActivity extends AppCompatActivity {
+
+    private ImageView close;
+    private ImageView image_added;
+    private TextView post;
+    AppCompatAutoCompleteTextView description;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        close =findViewById(R.id.close);
+        image_added = findViewById(R.id.image_added);
+        post = findViewById(R.id.post);
+        description = findViewById(R.id.description);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PostActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+        Crop
     }
 }
