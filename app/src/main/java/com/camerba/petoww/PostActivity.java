d154bac289d992a464f93f0ffde11f00355bdb88
mertkbkci <103898638+mertkbkci@users.noreply.github.com>
2023-06-08 19:01:46 +0300

    initial commit

--- app/src/main/java/com/camerba/petoww/PostActivity.java
+++ app/src/main/java/com/camerba/petoww/PostActivity.java
@@ -1,20 +1,36 @@
 package com.camerba.petoww;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.io.Files;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+
+
 public class PostActivity extends AppCompatActivity {
 
+    private Uri imageUri;
     private ImageView close;
     private ImageView image_added;
     private TextView post;
     AppCompatAutoCompleteTextView description;
+
 
 
     @Override
@@ -22,7 +38,7 @@ public class PostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
-        close =findViewById(R.id.close);
+        close = findViewById(R.id.close);
         image_added = findViewById(R.id.image_added);
         post = findViewById(R.id.post);
         description = findViewById(R.id.description);
@@ -34,6 +50,52 @@ public class PostActivity extends AppCompatActivity {
                 finish();
             }
         });
-        Crop
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                upload();
+            }
+        });
+
+    }
+
+    private void upload() {
+        ProgressDialog pd = new ProgressDialog(this);
+        pd.setMessage("Yükleniyor");
+        pd.show();
+
+        if (imageUri != null) {
+
+            StorageReference filePath = FirebaseStorage.getInstance().getReference("İlanlar").child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+            StorageTask uploadtask = filePath.putFile(imageUri);
+            uploadtask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return filePath.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    Uri downloadUri = task.getResult();
+                }
+            });
+        }
+
+    }
+
+    private String getFileExtension(Uri uri) {
+
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(this.getContentResolver().getType(uri));
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
     }
 }
